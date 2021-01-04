da5f738c403ae5d97d7848fc154e13c134faace7
kenny132652 <73508417+kenny132652@users.noreply.github.com>
2021-01-05 00:17:45 +0800

    Add files via upload

--- 20201229/mysql.php
+++ 20201229/mysql.php
@@ -1,5 +1,5 @@
 <?php
-$dbhost = ‘localhost’;  // mysql伺服器主機位址
+$dbhost = ‘nginx’;  // mysql伺服器主機位址
 $dbuser = ‘root’;            // mysql使用者名稱
 $dbpass = ‘123456’;          // mysql 密碼
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
